dbba10d294533f05b3fe0eaea8930bfd205d1c8c
Michael <v-duc@datasart.io>
2019-12-11 09:53:25 +0700

    update fzf

--- config/zsh/myalias.sh
+++ config/zsh/myalias.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env zsh
 
+# Setting rg as the default source for fzf
+export FZF_DEFAULT_COMMAND='rg --files'
+export FZF_DEFAULT_OPTS='--bind tab:down --cycle'
+# Apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# 
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
 # FZF FUNCTIONS ##
 # fo [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
+#
 fo() {
   local files
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
